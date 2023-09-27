6abca16de524f8a8479f82dc67440628dd14a953
Eugenyy Malgin <eugenimalgin@yandex.ru>
2023-09-27 18:42:01 +0300

    Change in properties

--- src/test/java/FooterTest.java
+++ src/test/java/FooterTest.java
@@ -19,102 +19,97 @@ public class FooterTest extends BaseTest {
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript("window.scrollBy(0, 1500)");
         footer.clickButtonPrivacyPolicy();
-        Assert.assertEquals(footer.checkURl(), "https://foundation.wikimedia.org/wiki/Policy:Privacy_policy");
+        Assert.assertEquals(footer.checkURl(),System.getProperty("urlPrivacyPolicy"));
     }
 
     @Test
     public void checkButtonWikipediaDescription() {
         LOG.info("Проверяем кнопку Описание Вкипедии");
         footer.clickButtonWikipediaDescription();
-        Assert.assertEquals(footer.checkURl(), "https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%" +
-                "D0%B4%D0%B8%D1%8F:%D0%9E%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlWikipediaDescription"));
     }
 
     @Test
     public void checkButtonDenialResponsibility() {
         LOG.info("Проверяем кнопку Отказ от ответсвенности");
         footer.clickButtonDenialResponsibility();
-        Assert.assertEquals(footer.checkURl(), "https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%" +
-                "D0%B4%D0%B8%D1%8F:%D0%9E%D1%82%D0%BA%D0%B0%D0%B7_%D0%BE%D1%82_%D0%BE%D1%82%D0%B2%D0%B5%D1%82%D1%81" +
-                "%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D1%81%D1%82%D0%B8");
+        Assert.assertEquals(footer.checkURl(),System.getProperty("urlDenialResponsibility") );
     }
 
     @Test
     public void checkButtonContactUs() {
         LOG.info("Проверяем кнопку Свяжитесь с нами");
         footer.clickButtonContactUs();
-        Assert.assertEquals(footer.checkURl(), "https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D0%BF%D0%B5%D" +
-                "0%B4%D0%B8%D1%8F:%D0%9A%D0%BE%D0%BD%D1%82%D0%B0%D0%BA%D1%82%D1%8B");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlContactUs"));
     }
 
     @Test
     public void checkButtonCodeConduct() {
         LOG.info("Проверяем кнопку Кодекс поведения");
         footer.clickButtonCodeConduct();
-        Assert.assertEquals(footer.checkURl(), "https://foundation.wikimedia.org/wiki/Policy:Universal_Code_of_Conduct");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlCodeConduct"));
     }
 
     @Test
     public void checkButtonMobileVersion() {
         LOG.info("Проверяем кнопку Мобильная версия");
         footer.clickButtonMobileVersion();
-        Assert.assertEquals(footer.checkURl(), "https://ru.m.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B" +
-                "2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlMobileVersion"));
     }
 
     @Test
     public void checkButtonDevelopers() {
         LOG.info("Проверяем кнопку Разработчики");
         footer.clickButtonDevelopers();
-        Assert.assertEquals(footer.checkURl(), "https://developer.wikimedia.org/");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlDevelopers"));
     }
 
     @Test
     public void checkButtonStatistics() {
         LOG.info("Проверяем кнопку Статистика");
         footer.clickButtonStatistics();
-        Assert.assertEquals(footer.checkURl(), "https://stats.wikimedia.org/#/ru.wikipedia.org");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlStatistics"));
     }
 
     @Test
     public void checkIconWikimediaFoundationIcon() {
         LOG.info("Проверяем иконку Фонд Википедия");
         footer.clickIconWikimediaFoundation();
-        Assert.assertEquals(footer.checkURl(), "https://wikimediafoundation.org/");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlWikimediaFound"));
     }
 
     @Test
     public void checkButtonTermsUse() {
         LOG.info("Проверяем кнопку Условия использования");
         footer.clickButtonTermsUse();
-        Assert.assertEquals(footer.checkURl(), "https://foundation.wikimedia.org/wiki/Policy:Terms_of_Use/ru");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlTermsUse"));
     }
 
     @Test
     public void checkButtonWikimediaFoundation() {
         LOG.info("Проверяем кнопку Фонд Википедия");
         footer.clickButtonWikimediaFoundation();
-        Assert.assertEquals(footer.checkURl(), "https://wikimediafoundation.org/ru/");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlWikimediaFoundRU"));
     }
 
     @Test
     public void checkButtonLicense() {
         LOG.info("Проверяем кнопку Лицензия");
         footer.clickButtonLicense();
-        Assert.assertEquals(footer.checkURl(), "https://creativecommons.org/licenses/by-sa/4.0/deed.ru");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlLicense"));
     }
 
     @Test
     public void checkIconMediaWiki() {
         LOG.info("Проверяем иконку Медия Вики");
         footer.clickIconMediaWiki();
-        Assert.assertEquals(footer.checkURl(), "https://www.mediawiki.org/wiki/MediaWiki");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlMediaWiki"));
     }
 
     @Test
     public void checkButtonCookieStatement() {
         LOG.info("Проверяем кнопку заявление о куки");
         footer.clickButtonCookieStatement();
-        Assert.assertEquals(footer.checkURl(), "https://foundation.wikimedia.org/wiki/Policy:Cookie_statement");
+        Assert.assertEquals(footer.checkURl(), System.getProperty("urlCookieStatement"));
     }
 }
